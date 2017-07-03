1dce99385bc18394c935f6a64512ca1dc00f649c
Andrey Drenski <andreshko.dre@gmail.com>
2017-07-04 01:17:13 +0300

    added UTF-8 support

--- psst.hs
+++ psst.hs
@@ -1,8 +1,11 @@
 module Main (main) where
 
+import Prelude hiding (lines, unlines, readFile, writeFile)
 import System.Environment (getArgs)
 import System.Random (mkStdGen, randomRs, random, StdGen(..))
-import System.IO (readFile, writeFile)
+import qualified Data.ByteString as B (readFile, writeFile)
+import Data.Text (pack, unpack, lines, unlines)     -- requires "text" package
+import Data.Text.Encoding (encodeUtf8, decodeUtf8)  -- requires "text" package 
 import Control.Monad (liftM)
 
 main = do
@@ -11,13 +14,10 @@ main = do
         putStrLn "Inappropriate number of arguments!"
     else do
         let (fileName, k) = (head args, read (head (tail args)) :: Int)
-        (s:f) <- liftM lines $ readFile fileName
-        let (seed, names, maxIdx) = (read s :: Int, f, (length f) - 1)
+        (s:f) <- liftM (lines . decodeUtf8) $ B.readFile fileName
+        let (seed, names, maxIdx) = (read $ unpack s :: Int, f, (length f) - 1)
             gen = mkStdGen seed
-            chosen = map (names!!) $ take k $ randomRs (0, maxIdx) gen
+            chosen = map (names!!) . take k $ randomRs (0, maxIdx) gen
             (newSeed, _) = random gen :: (Int, StdGen)
-        mapM_ putStrLn chosen
-        writeFile fileName $ unlines $ (show newSeed):names
-        
-
-        
+        mapM_ (putStrLn . unpack) chosen
+        B.writeFile fileName . encodeUtf8 . unlines $ (pack $ show newSeed):names
