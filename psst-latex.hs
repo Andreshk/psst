c344a95fcf428becf4d5f47daa7152f58c076170
Andrey Drenski <andreshko.dre@gmail.com>
2017-07-04 20:53:11 +0300

    Add files via upload

---
+++ psst-latex.hs
@@ -0,0 +1,33 @@
+module Main (main) where
+
+import Prelude hiding (lines, unlines, readFile, writeFile)
+import System.Environment (getArgs, withArgs)
+import System.Random (mkStdGen, randomRs, random, StdGen(..))
+import qualified Data.ByteString as B (readFile, writeFile)
+import Data.Text (Data.Text(..), pack, unpack, lines, unlines, splitOn)     -- requires "text" package
+import Data.Text.Encoding (encodeUtf8, decodeUtf8)  -- requires "text" package 
+import Control.Monad (liftM)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    if length args < 3 then
+        putStrLn "Inappropriate number of arguments!"
+    else do
+        let (fileName:templateName:resultName:_) = args
+        (s:names) <- liftM (lines . decodeUtf8) $ B.readFile fileName
+        let (seed, maxIdx) = (read $ unpack s :: Int, (length names) - 1)
+            gen = mkStdGen seed
+            message = pack "Псст! Сесия е. Вземи си изпит!"
+            chosen = map (names!!) . take 17 $ randomRs (0, maxIdx) gen
+            (newSeed, _) = random gen :: (Int, StdGen)
+        latex <- liftM (splitOn (pack "--split") . decodeUTF8) $ B.readFile templateName
+        --mapM_ (putStrLn . unpack) chosen
+        B.writeFile resultName . encodeUtf8 $ buildLatex latex (message:chosen)
+        B.writeFile fileName . encodeUtf8 . unlines $ (pack $ show newSeed):names
+        putStrLn "Done!"
+
+buildLatex :: [Data.Text] -> [Data.Text] -> Data.Text
+buildLatex (str:[]) [] = str
+buildLatex (x:xs) (y:ys) = x `append` y `append` (f xs ys)
+buildLatex _ _ = error "# of slots /= # replacements!!1!1"
