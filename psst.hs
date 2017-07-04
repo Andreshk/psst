2f9fa6179b25da57c5e708fe83fb0add50ae043d
Andrey Drenski <andreshko.dre@gmail.com>
2017-07-05 01:10:38 +0300

    consistent with -latex version

--- psst.hs
+++ psst.hs
@@ -2,26 +2,33 @@ module Main (main, psst) where
 
 import Prelude hiding (lines, unlines, readFile, writeFile)
 import System.Environment (getArgs, withArgs)
-import System.Random (mkStdGen, randomRs, random, StdGen(..))
+import System.Random (StdGen(..), mkStdGen, randomRs, random)
 import qualified Data.ByteString as B (readFile, writeFile)
-import Data.Text (pack, unpack, lines, unlines)     -- requires "text" package
+import Data.Text (Text(..), pack, unpack, lines, unlines)   -- requires "text" package
 import Data.Text.Encoding (encodeUtf8, decodeUtf8)  -- requires "text" package 
 import Control.Monad (liftM)
 
 main = do
-    args <- getArgs
-    if length args < 2 then
-        putStrLn "Inappropriate number of arguments!"
-    else do
-        let (fileName, k) = (head args, read (head (tail args)) :: Int)
-        (s:f) <- liftM (lines . decodeUtf8) $ B.readFile fileName
-        let (seed, names, maxIdx) = (read $ unpack s :: Int, f, (length f) - 1)
-            gen = mkStdGen seed
-            chosen = map (names!!) . take k $ randomRs (0, maxIdx) gen
-            (newSeed, _) = random gen :: (Int, StdGen)
-        mapM_ (putStrLn . unpack) chosen
-        B.writeFile fileName . encodeUtf8 . unlines $ (pack $ show newSeed):names
+    (fileName, k) <- parseArgs
+    (seed:names) <- liftM (lines . decodeUtf8) $ B.readFile fileName
+    let gen = mkStdGen . read . unpack $ seed
+        chosen = map (names!!) $ randomRs (0, (length names) - 1) gen
+        newSeed = fst $ random gen :: Int
+    mapM_ (putStrLn . unpack) (take k chosen)
+    textToFile fileName (unlines $ (pack $ show newSeed):names)
 
 -- User-friendly version - to be called from GHCI or WinGHCI
 psst :: String -> Int -> IO ()
 psst fileName k = withArgs [fileName, show k] main
+
+parseArgs :: IO (String, Int)
+parseArgs = do
+    args <- getArgs
+    if length args < 2 then error "Inappropriate number of arguments!"
+    else let (a:b:_) = args in return (a, read b)
+
+textToFile :: String -> Text -> IO ()
+textToFile fileName text = B.writeFile fileName (encodeUtf8 text)
+
+fileToText :: String -> IO Text
+fileToText fileName = liftM decodeUtf8 $ B.readFile fileName
