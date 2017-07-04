9cbd93fa8d5bed96cc4ac9133d0150c39cc95c45
Andrey Drenski <andreshko.dre@gmail.com>
2017-07-04 22:16:32 +0300

    finished.

--- psst-latex.hs
+++ psst-latex.hs
@@ -1,33 +1,38 @@
 module Main (main) where
 
 import Prelude hiding (lines, unlines, readFile, writeFile)
-import System.Environment (getArgs, withArgs)
-import System.Random (mkStdGen, randomRs, random, StdGen(..))
+import System.Environment (getArgs)
+import System.Random (StdGen(..), mkStdGen, randomRs, random)
 import qualified Data.ByteString as B (readFile, writeFile)
-import Data.Text (Text(..), pack, unpack, lines, unlines, splitOn, append)     -- requires "text" package
+import Data.Text (Text(..), pack, unpack, lines, unlines, splitOn, append)  -- requires "text" package
 import Data.Text.Encoding (encodeUtf8, decodeUtf8)  -- requires "text" package 
 import Control.Monad (liftM)
 
 main :: IO ()
 main = do
+    (fileName, templateName, resultName) <- parseArgs
+    (seed:names) <- liftM lines $ fileToText fileName
+    let gen = mkStdGen . read . unpack $ seed
+        message = pack "Псст! Сесия е. Вземи си изпит!"
+        chosen = map (names!!) $ randomRs (0, (length names) - 1) gen
+        newSeed = fst $ random gen :: Int
+    pieces <- liftM (splitOn (pack "--split")) $ fileToText templateName
+    textToFile resultName (buildLatex pieces (message:chosen))
+    textToFile fileName (unlines $ (pack $ show newSeed):names)
+    putStrLn "Done!"
+
+parseArgs :: IO (String, String, String)
+parseArgs = do
     args <- getArgs
-    if length args < 3 then
-        putStrLn "Inappropriate number of arguments!"
-    else do
-        let (fileName:templateName:resultName:_) = args
-        (s:names) <- liftM (lines . decodeUtf8) $ B.readFile fileName
-        let (seed, maxIdx) = (read $ unpack s :: Int, (length names) - 1)
-            gen = mkStdGen seed
-            message = pack "Псст! Сесия е. Вземи си изпит!"
-            chosen = map (names!!) . take 17 $ randomRs (0, maxIdx) gen
-            (newSeed, _) = random gen :: (Int, StdGen)
-        latex <- liftM (splitOn (pack "--split") . decodeUtf8) $ B.readFile templateName
-        --mapM_ (putStrLn . unpack) chosen
-        B.writeFile resultName . encodeUtf8 $ buildLatex latex (message:chosen)
-        B.writeFile fileName . encodeUtf8 . unlines $ (pack $ show newSeed):names
-        putStrLn "Done!"
+    if length args < 3 then error "Inappropriate number of arguments!"
+    else let (a:b:c:_) = args in return (a,b,c)
 
 buildLatex :: [Text] -> [Text] -> Text
-buildLatex (str:[]) [] = str
+buildLatex (str:[]) _ = str
 buildLatex (x:xs) (y:ys) = x `append` y `append` (buildLatex xs ys)
-buildLatex _ _ = error "# of slots /= # replacements!!1!1"
+
+textToFile :: String -> Text -> IO ()
+textToFile fileName text = B.writeFile fileName (encodeUtf8 text)
+
+fileToText :: String -> IO Text
+fileToText fileName = liftM decodeUtf8 $ B.readFile fileName
